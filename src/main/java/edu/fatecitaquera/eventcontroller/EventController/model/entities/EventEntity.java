b37184c6e4a68ee64ea5e9f17d224c477f9da05f
vitormneris <joaomoreiraneris0@gmail.com>
2024-10-14 20:06:38 -0300

    De Date para String, vai facilitar na transação de dados, guardar informação em tipo  Date não é muito útil neste caso.

--- src/main/java/edu/fatecitaquera/eventcontroller/EventController/model/entities/EventEntity.java
+++ src/main/java/edu/fatecitaquera/eventcontroller/EventController/model/entities/EventEntity.java
@@ -1,14 +1,11 @@
 package edu.fatecitaquera.eventcontroller.EventController.model.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.Date;
 
 @Getter
 @Setter
@@ -19,8 +16,6 @@ public class EventEntity {
     @Id
     private String id;
     private String name;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-    private Date startEvent;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-    private Date finishEvent;
+    private String startEvent;
+    private String finishEvent;
 }
